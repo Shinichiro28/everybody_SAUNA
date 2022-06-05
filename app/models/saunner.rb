e204ede1063902fd99df4339141742a3d37ad274
Shinichiro28 <s.erk.ikt@gmail.com>
2022-06-05 01:51:24 +0000

    [add]enum

--- app/models/saunner.rb
+++ app/models/saunner.rb
@@ -3,4 +3,12 @@ class Saunner < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :favorites, dependent: :destroy
+  has_many :chats, dependent: :destroy
+  has_many :chat_group_users, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :posts, dependent: :destroy
+
+
 end
