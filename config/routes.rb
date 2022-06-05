96bfe36e3e3ac835944e9350c3ce733ff248ee2f
Shinichiro28 <s.erk.ikt@gmail.com>
2022-06-05 03:39:35 +0000

    [add]routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
@@ -7,5 +8,33 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
+
+  root to: "public/homes#top"
+  get "/about" => "public/homes#about", as: "about"
+
+  get "/saunners/my_page" => "public/saunners#show", as: "my_page"
+  get "/saunners/edit" => "public/saunners#edit", as: "edit_saunners"
+  patch "saunners" => "public/saunners#update"
+  get "/saunners/unsubscribe" => "public/saunners#unsubscribe", as: "unsubscribe"
+  patch "/saunners/withdrawal" => "public/saunners#withdrawal", as: "withdrawal"
+
+  resources :saunas, controller: "public/saunas"
+  resources :posts, controller: "public/posts"
+  resources :favorites, only:[:create,:destroy], controller: "public/favorites"
+  resources :comments, only:[:create,:destroy], controller: "public/comments"
+  resources :chats, only:[:index], controller: "public/chats"
+  resources :chat_groups, expect:[:edit,:update], controller: "public/chat_groups"
+
+
+  namespace :admin do
+    resources :saunners, expect:[:new,:create,:destroy]
+    resources :saunas, expect:[:new,:create]
+    resources :posts, only:[:index,:show,:destroy]
+    resources :comments, only:[:index,:show,:destroy]
+  end
+
+  get "/admin" => "admin/homes#top", as: "admin"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
