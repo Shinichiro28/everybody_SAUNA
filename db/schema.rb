bbf17f0069b3e45d5750e92b2234eea99430abb4
Shinichiro28 <s.erk.ikt@gmail.com>
2022-06-05 03:38:19 +0000

    [add]saunners table  column nickname

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_04_143809) do
+ActiveRecord::Schema.define(version: 2022_06_05_030410) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -153,6 +153,7 @@ ActiveRecord::Schema.define(version: 2022_06_04_143809) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "is_deleted", default: false
+    t.string "nickname", null: false
     t.index ["email"], name: "index_saunners_on_email", unique: true
     t.index ["reset_password_token"], name: "index_saunners_on_reset_password_token", unique: true
   end
