cd2fd2b0b52d03a9642bd7312adb4b82baf00f7e
Shinichiro28 <s.erk.ikt@gmail.com>
2022-06-21 08:03:02 +0000

    [add]favorite非同期

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,15 +1,20 @@
 class Public::FavoritesController < ApplicationController
 
+  #非同期でリダイレクト先は削除
+
   def create
+    @sauna = Sauna.find(params[:sauna_id])
+    @post = Post.find(params[:post_id])
     @sauna_post_favorite = Favorite.new(saunner_id: current_saunner.id, sauna_id: params[:sauna_id], post_id: params[:post_id])
     @sauna_post_favorite.save
-    redirect_to sauna_post_path(params[:sauna_id], params[:post_id])
+
   end
 
   def destroy
+    @sauna = Sauna.find(params[:sauna_id])
+    @post = Post.find(params[:post_id])
     @sauna_post_favorite = Favorite.find_by(saunner_id: current_saunner.id, sauna_id: params[:sauna_id], post_id: params[:post_id])
     @sauna_post_favorite.destroy
-    redirect_to sauna_post_path(params[:sauna_id], params[:post_id])
   end
 
 end
