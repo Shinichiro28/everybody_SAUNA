82781389f2b113ae123eec5c835e93dec43e6fa4
Shinichiro28 <s.erk.ikt@gmail.com>
2022-06-27 13:09:24 +0000

    [add]get_store_image to Admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,11 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #saunaモデルの投稿写真
+  def get_store_image
+      (store_image.attached?) ? store_image : 'no_store.jpeg'
+  end
+
+
 end
