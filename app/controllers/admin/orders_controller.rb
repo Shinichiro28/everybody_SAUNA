f52be4d015910355359f0017abb5d8a4fca36179
Shinichiro28 <s.erk.ikt@gmail.com>
2022-07-28 13:59:13 +0000

    [add] Order managiment function

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,9 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
 
   def show
+    @order = Order.find(params[:id])
+    @order_products = @order.order_products
+    @subtotal = 0
   end
 
   def index
@@ -9,6 +12,19 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    if @order.order_products.update_all(production_status: 1)
+      @order.order_products == "waiting"
+    end
+    flash[:notice] = "更新完了しました"
+    redirect_to request.referer
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 
 end
