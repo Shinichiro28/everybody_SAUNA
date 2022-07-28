fe1ed98b7142988e06dae658070a984f76d3fd7f
Shinichiro28 <s.erk.ikt@gmail.com>
2022-07-28 17:03:09 +0000

    [add] Product my_list/detail

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,9 +6,13 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_products = @order.order_products
+    @subtotal = 0
   end
 
   def index
+    @orders = Order.page(params[:page]).per(10).order(created_at: :desc)
   end
 
   def create
