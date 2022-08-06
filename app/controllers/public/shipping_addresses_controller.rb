841e3b8546cbfc5009cf75186ea56f056b3b5500
Shinichiro28 <s.erk.ikt@gmail.com>
2022-08-06 14:45:36 +0000

    [add] Shipping_address list

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,18 +1,48 @@
 class Public::ShippingAddressesController < ApplicationController
+  before_action :authenticate_saunner!
 
   def index
+    @shipping_address = ShippingAddress.new
+    @shipping_addresses = current_saunner.shipping_addresses.all
   end
 
   def edit
+    @shipping_address = ShippingAddress.find(params[:id])
   end
 
   def create
+    @shipping_address = ShippingAddress.new(shipping_address_params)
+    @shipping_address.saunner_id = current_saunner.id
+    if @shipping_address.save
+      redirect_to shipping_addresses_path, notice: "配送先住所を登録しました"
+    else
+      @shipping_address = current_saunner.shipping_addresses.all
+      render 'index'
+    end
   end
 
   def update
+    @shipping_address = ShippingAddress.find(params[:id])
+    if @shipping_address.update(shipping_address_params)
+      redirect_to shipping_addresses_path, notice: "配送先を編集しました"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @shipping_address = ShippingAddress.find(params[:id])
+    if @shipping_address.destroy
+      redirect_to shipping_addresses_path, notice: "配送先住所を削除しました"
+    end
   end
+
+
+  private
+
+  def shipping_address_params
+    params.require(:shipping_address).permit(:postcode, :address, :name, :saunner_id)
+  end
+
 
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,13 @@
 class ShippingAddress < ApplicationRecord
+
+  belongs_to :saunner
+
+  def address_display
+    '〒' + postcode + ' ' + address + ' ' + name
+  end
+
+  validates :postcode, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+
 end
